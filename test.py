682b4e102850b5dd9d4c5cda8a0b1445a2dd56d9
Tyler Reddy <tyler.je.reddy@gmail.com>
2023-03-06 15:12:50 -0700

    ENH: more reliable IO
    * force-flushing/syncing to a file allows eBPF
    to more consistenly count POSIX write() events
    than terminal printing did

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 import os
 
-for i in range(1_000_000):
-    print("boo!", flush=True)
+with open("test_out.txt", "w") as outfile:
+    for i in range(1_000):
+        outfile.write("a")
+        outfile.flush()
+        os.fsync(outfile)
 
 print("PID:", os.getpid())
